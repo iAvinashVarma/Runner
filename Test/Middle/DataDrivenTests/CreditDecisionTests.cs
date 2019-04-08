7b6c82693d0a9f86c5f52b39cad9c4affb4a7e1a
Avinash Varma <avinashvarma@live.com>
2019-04-08 21:29:47 +0800

    Add DataDrivenTests.

---
+++ Test/Middle/DataDrivenTests/CreditDecisionTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace DataDrivenTests
+{
+	[TestFixture]
+	public class CreditDecisionTests
+	{
+		public static IEnumerable<TestCaseData> CreditDecisionTestData
+		{
+			get
+			{
+				yield return new TestCaseData(100, "Declined");
+				yield return new TestCaseData(549, "Declined");
+				yield return new TestCaseData(550, "Maybe");
+				yield return new TestCaseData(674, "Maybe");
+				yield return new TestCaseData(675, "We look forward  to doing business with you!");
+			}
+		}
+
+		[TestCaseSource(typeof(CreditDecisionTests), "CreditDecisionTestData")]
+		public void MakeCreditDecision_Always_ReturnsExpectedResult(int creditScore, string expectedResult)
+		{
+			Assert.That(creditScore > 0);
+		}
+	}
+}
---
+++ Test/Middle/DataDrivenTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataDrivenTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataDrivenTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a62bb78a-40c2-4bee-ae3f-92a1782e83ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
